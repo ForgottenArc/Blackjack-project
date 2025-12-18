5011de3129bf7b73a0778f833d72cc4d73eb4b98
Jerry Lin <jerryxue0717@gmail.com>
2025-12-17 22:50:13 -0500

    feat: added the comparision and determination of winning . Fixed the bug of getting random cards and summing it up from list

--- main.py
+++ main.py
@@ -1,79 +1,69 @@
 
 import art
 import random
-         #A                       #J,Q,K
-cards = {
-        "A":11,
-        "2":2,
-        "3":3,
-        "4":4,
-        "5":5,
-        "6":6,
-        "7":7,
-        "8":8,
-        "9":9,
-        "J":10,
-        "Q":10,
-        "K":10
-}
 
-def random_input(cards):
-    input_storing=[]
-    for keys in cards:
-       input_storing.append(keys)
-
-    return random.choice(input_storing)
-          
-user_card= []
-
-bot_card = []
-
-get_first = str(random_input(cards))
-get_second = str(random_input(cards))
-get_third = str(random_input(cards))
-get_forth = str(random_input(cards))
-
-#The user and computer should
-#each get 2 random cards
+        #A                                J  Q    K
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 
+def deal_card(cards):
+    return random.choice(cards)
 
 
-user_card1 = cards[get_first]
-user_card.append(user_card1)
-user_card2 =cards[get_second]
-user_card.append(user_card2)
+user_cards = []
+computer_cards = []
 
-bot_card1 = cards[get_third]
-bot_card.append(bot_card1)
-bot_cards2 = cards[get_forth]
-bot_card.append(bot_cards2)
+for i in range(2):
+    user_cards.append(deal_card(cards))
+    computer_cards.append(deal_card(cards))
+
+#calculating the scores
+def calculate_score(cards):
+
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+
+    return sum(cards)
+
+user_result = calculate_score(user_cards)
+bot_result = calculate_score(computer_cards)
+
+if bot_result == 0 :
+    print("Bot win BlackJack")
+    exit()
+
+if user_result == 0:
+    print("User win BlackJack")
+    exit()
+
+if user_result == 21:
+    print(f"User win with the result of {user_result}")
+    exit()
+
+if bot_result == 21 :
+    print(f"bot win with the result of {bot_result}")
+    exit()
+
+if user_result < 21 and bot_result < 21:
+
+    if user_result == bot_result :
+        print(f"Tied with the result of {user_result}")
 
 
-user_total = user_card[0] + user_card[1]
-bot_total = bot_card[0] + bot_card[1]
+    elif user_result > bot_result:
+        print(f"User wins with the result of : {user_result} and Bot has {bot_result}")
+
+    elif bot_result > user_result :
+        print(f"Bot win with the result of : {bot_result} and User has {user_result}")
+
+else:
+    print("Busted!!!!")
 
 
-def battle():
-
-
-
-    if user_total >=21:
-        print("You lose BUSTED")
-        return
-    
-    if bot_total >=21:
-        print("Bot lose BUSTED")
-        return
-
-    if user_total > bot_total :
-        print(f"User wins the total is {user_total}, and the bot is {bot_total}")
-    elif user_total == bot_total:
-        print(f"tie the value is {user_total}, bot value is {bot_total}")
-    else:
-        print(f"The bot wins the total is {bot_total} and User is {user_total}")
-
-battle()
 
 
 
